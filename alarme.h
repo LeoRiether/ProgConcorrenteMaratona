4444ed31e995a6c0541f7f4488de034c5620b1f1
LeoRiether <leonardoalves8217@gmail.com>
2021-10-11 17:33:09 -0300

    Checkpoint

--- alarme.h
+++ alarme.h
@@ -7,6 +7,7 @@
 #include <pthread.h>
 
 #include "cond.h"
+#include "deque.h"
 
 #define MAX_TIMERS 64
 int next_timer_id;
@@ -17,6 +18,8 @@ typedef struct alarme_arg {
 	int delay;
 	int id;
 	cond_t* c;
+	deque_t* d;
+	void* d_element;
 } alarme_arg;
 
 void* alarme_thread(void* arg) {
@@ -25,31 +28,42 @@ void* alarme_thread(void* arg) {
 	sleep(a->delay);
 
 	m_lock(&a->c->lock);
-	if (!alarme_cancelado[a->id])
-		pthread_cond_broadcast(&a->c->cond);
+	if (!alarme_cancelado[a->id]) {
+		// Coloca o elemento no deque
+		if (a->d) d_push_back(a->d, a->d_element);
+
+		pthread_cond_signal(&a->c->cond);
+	}
 	m_unlock(&a->c->lock);
 
 	free(arg);
 	pthread_exit(0);
 }
 
-// Cria uma thread que dorme por `delay` segundos, e logo depois dá um broadcast
-// na variável do condição dada. O alarme pode ser cancelado ao setar cancelado[id] = true,
-// tomando cuidado para estar de posse do lock c->lock para não ter condição de corrida.
+// Cria uma thread que dorme por `delay` segundos, e logo depois dá um signal
+// na variável de condição dada.
+// Além disso, se for dado um deque_t* d não nulo, antes do signal o elemento `d_element`
+// será colocado no deque.
+// O alarme pode ser cancelado ao setar `alarme_cancelado[id] = true`, tomando cuidado para estar
+// de posse do lock c->lock, de modo que não haja condição de corrida.
 // A função retorna o `id` do alarme gerado.
-int alarme(int delay, cond_t* c) {
+int alarme(int delay, cond_t* c, deque_t* d, void* d_element) {
 
 	// Cria os argumentos da thread de alarme
 	alarme_arg* arg = (alarme_arg*)malloc(sizeof(alarme_arg));
-	arg->delay = delay;
-	arg->c = c;
+	*arg = (alarme_arg) {
+		.delay = delay,
+		.c = c,
+		.d = d,
+		.d_element = d_element
+	};
 
 	m_lock(&next_timer_lock);
-	int id = next_timer_id++;
-	arg->id = id;
-	if (next_timer_id >= MAX_TIMERS) // os ids sempre estão em [0..MAX_TIMERS)
-		next_timer_id -= MAX_TIMERS;
-	alarme_cancelado[id] = false;
+		int id = next_timer_id++;
+		arg->id = id;
+		if (next_timer_id >= MAX_TIMERS) // os ids sempre estão em [0..MAX_TIMERS)
+			next_timer_id -= MAX_TIMERS;
+		alarme_cancelado[id] = false;
 	m_unlock(&next_timer_lock);
 
 	// Cria a thread
--- main.c
+++ main.c
@@ -17,28 +17,49 @@
 
 #define N (TIMES * SZ_TIME)
 
+// gera um número aleatório em [low, high]
+static inline int rand_int(int low, int high) {
+	return rand() % (high - low + 1) + low;
+}
+
+static inline long long pair(int x, int y) {
+	return ((long long)x << 32ll) | y;
+}
+
 typedef enum tipo_evento_t {
-	Acordar, Scoreboard
+	Alarme, Placar
 } tipo_evento_t;
 
 typedef struct evento_t {
 	tipo_evento_t tipo;
-	void* payload;
+	int payload;
 } evento_t;
 
+typedef enum competidor_st {
+	Pensando, AchouSolucao, Escrevendo, OlhandoPlacar,
+} competidor_st;
+
+competidor_st state[N]; // estado dos competidores
 cond_t sinal[N];
-evento_t evento[N]; //! devia ser uma fila ou stack de eventos
+deque_t eventos[N]; // eventos[i] = fila de eventos do i-ésimo competidor
+					// para acessar eventos[i], deve-se estar de posse do lock sinal[i].lock
 pthread_barrier_t comeco_da_prova;
+pthread_mutex_t computador;
 
 void* competidor(void*);
 
 int main(int argv, char* argc[]) {
 
-	pthread_barrier_init(&comeco_da_prova, 0, N);
+	srand(time(NULL));
 
+	// Inicializa as variáveis
 	pthread_t competidores[N];
+	pthread_barrier_init(&comeco_da_prova, 0, N);
+	pthread_mutex_init(&computador, 0);
 	for (int i = 0; i < N; i++) {
 		c_init(&sinal[i]);
+		d_init(&eventos[i]);
+		state[i] = Pensando;
 	}
 
 	for (int team = 0; team < TIMES; team++) {
@@ -55,9 +76,34 @@ int main(int argv, char* argc[]) {
 		pthread_join(competidores[i], NULL);
 	}
 
+	// Destrói as variáveis
+	pthread_barrier_destroy(&comeco_da_prova);
+	pthread_mutex_destroy(&computador);
+	for (int i = 0; i < N; i++) {
+		c_destroy(&sinal[i]);
+		d_destroy(&eventos[i]);
+	}
+
 	return 0;
 }
 
+int acordar_em(int delay, int id) {
+	// Colocamos um alarme para daqui a `delay` segundos.
+	// Passado esse tempo, será dado um signal em `sinal[id]` e
+	// será colocado o elemento `evt` no deque `eventos[i]`.
+	evento_t* evt = (evento_t*)malloc(sizeof(evento_t));
+	*evt = (evento_t) {
+		.tipo = Alarme,
+		.payload = 0,
+	};
+	int id_alarme = alarme(delay, &sinal[id], &eventos[id], evt);
+	return id_alarme;
+}
+
+void achou_solucao(int id);
+void olhar_placar(int id);
+
+// Implementação do comportamento de um competidor
 void* competidor(void* arg) {
 	int id = *(int*)arg;
 	printf("Competidor %d entrou na sala\n", id);
@@ -66,15 +112,71 @@ void* competidor(void* arg) {
 	// ao mesmo tempo
 	pthread_barrier_wait(&comeco_da_prova);
 
-	m_lock(&sinal[id].lock);
-	alarme(2, &sinal[id]);
-	printf("%d esperando o alarme...\n", id);
-	pthread_cond_wait(&sinal[id].cond, &sinal[id].lock);
-	printf("%d recebeu o alarme!\n", id);
-	m_unlock(&sinal[id].lock);
+	for (;;) {
+		acao_t prox_acao = rand() % 2;
+
+		printf("%d está pensando em um problema\n", id);
+		sleep(rand_int(4, 7));
+
+		switch (prox_acao) {
+			case AchouSolucao:
+				printf("%d encontrou a solução para um problema!\n", id);
+				achou_solucao(id);
+				break;
+			case OlharPlacar:
+				printf("%d quer olhar o placar\n", id);
+				olhar_placar(id);
+				break;
+			default:
+				ensure(false, "prox_acao inválida");
+		}
+	}
 
 	printf("Competidor %d saiu da sala\n", id);
 
 	free(arg);
 	pthread_exit(0);
+}
+
+// O competidor `id` encontrou a solução de um problema, e agora quer ficar de posse do computador
+// para escrever o código.
+void achou_solucao(int id) {
+	if (pthread_mutex_trylock(&computador) == 0) {
+		printf("%d está de posse do computador, escrevendo uma solução\n", id);
+		return;
+	}
+
+	int id_alarme = acordar_em(2, id);
+
+	printf("%d está resolvendo uma questão...\n", id);
+
+	m_lock(&sinal[id].lock);
+		for (;;) {
+			// Esperamos um sinal
+			pthread_cond_wait(&sinal[id].cond, &sinal[id].lock);
+
+			// Pegamos o primeiro evento do deque
+			void* evt_pointer = d_pop_front(&eventos[id]);
+			evento_t evt = *(evento_t*)evt_pointer;
+			free(evt_pointer);
+
+			// E tratamos o evento de acordo
+			if (evt.tipo == Alarme) {
+				// O competidor acabou de escrever o código da questão
+				// submit();
+				// libera_pc()
+				break;
+			} else if (evt.tipo == Placar) {
+
+			}
+		}
+
+		// TODO: tomar cuidado com outros eventos que podem ter aparecido e ainda estão no deque
+
+		printf("%d terminou de escrever sua solução\n", id);
+	m_unlock(&sinal[id].lock);
+}
+
+void olhar_placar(int id) {
+	printf("olhar_placar ainda não foi implementado\n");
 }
